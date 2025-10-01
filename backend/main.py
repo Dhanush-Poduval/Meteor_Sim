b7320fe5885bf5ac7883c3f9aa7f9a81764c51ee
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-10-01 17:21:50 +0530

    feat:made the meteor endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi.params import Form
 app=FastAPI()
 
 @app.get("/")
 def test():
     return "Test successfull"
+
+@app.post('/meteor')
+def meteor(name:str=Form(...),speed:float=Form(...),mass:float=Form(...),radius:float=Form(...)):
+    return{
+        'name':name,
+        'speed':speed,
+        'mass':mass,
+        'radius':radius
+    }
